e64d0153cd9b9e7abd116db073bf91db497d4a05
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-11-28 19:37:28 -0800

    working with assets

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>@yield('title')</title>
+        @viteReactRefresh
+        @vite(['resources/css/app.css', 'resources/js/app.js', 'react/src/main.jsx'])
     </head>
 
     <body>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'MySite.com - Home')
 
 @section('content')
+    <div id="root"></div>
     <h1>Welcome Back, {{ $user }}. You are {{ $age }} and live in {{ $city }}</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis, perspiciatis, necessitatibus magni ad eaque voluptas dolorum, explicabo sed illum repellat fugiat soluta qui sequi? Neque fugit veritatis molestiae rerum voluptate!</p>
     <ul>
